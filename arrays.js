8bccbe4804531b70f32bf9c6522984a0fcf8dcf4
Mary Kang <mary.kang09@gmail.com>
2020-02-11 17:57:06 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ['snickers','hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray (array,element){
-  var array = ["array"]
-  array.unshift("element")
+  var array = [1]
+  array.unshift("foo")
   return array
 }
