597c5d7a304168dc02fb26b61264613eba971373
Mary Kang <mary.kang09@gmail.com>
2020-02-11 17:30:39 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,10 @@
+var chocolateBars = ['snickers','hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray (){
+var array = [ ]
+return array.unshift()
+}
+
+function destructivelyAddElementToBeginningOfArray (){
+
+}
