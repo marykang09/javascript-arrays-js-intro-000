ff6fba496b839f9e5ee52e31b4427948b53f9698
Mary Kang <mary.kang09@gmail.com>
2020-02-12 01:50:27 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -31,7 +31,7 @@ function accessElementInArray(array, index){
 
 function destructivelyRemoveElementFromBeginningOfArray (array){
   var array = [1,2,3];
-  array.shift();
+  array.shift(0);
   return array
 }
 
