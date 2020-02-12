5ea927277c2a52c41073fcfa55b2e7ff7da8262d
Mary Kang <mary.kang09@gmail.com>
2020-02-12 02:23:02 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -9,7 +9,6 @@ function addElementToBeginningOfArray (array,element){
 function destructivelyAddElementToBeginningOfArray (array,element){
   var array = [1]
   ["foo", ...array]
-  array = ["foo", ...array]
   return array
 }
 
