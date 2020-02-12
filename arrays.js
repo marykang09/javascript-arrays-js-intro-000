2e29988ae8cc3a7ec461a2df019fc5477716bd34
Mary Kang <mary.kang09@gmail.com>
2020-02-12 03:26:02 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -43,7 +43,7 @@ function removeElementFromBeginningOfArray(array){
 
 function destructivelyRemoveElementFromEndOfArray(array){
   var array = [1,2,3];
-  array.shift(2)
+  array.shift(1)
   return array
 }
 
