484275622a5ca3345bb47696748aaf0c6c8c1c0e
Mary Kang <mary.kang09@gmail.com>
2020-02-12 03:40:13 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -8,7 +8,8 @@ function addElementToBeginningOfArray (array,element){
 
 function destructivelyAddElementToBeginningOfArray (array,element){
   var array = [1];
-  return ["foo", ...array];
+  ["foo", ...array];
+  return array
 }
 
 function addElementToEndOfArray (array, element){
