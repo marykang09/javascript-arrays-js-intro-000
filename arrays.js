92449ec9fac8d7c5d3887ae66e3283cc450e2c7f
Mary Kang <mary.kang09@gmail.com>
2020-02-12 01:56:26 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -48,5 +48,7 @@ function destructivelyRemoveElementFromEndOfArray(array){
 }
 
 function removeElementFromEndOfArray(array){
-
+var array = [1,2,3]
+array.slice(0, array.length - 1)
+return array
 }
