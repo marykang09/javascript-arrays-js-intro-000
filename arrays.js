4a30f615f6528a7239d9f0bacfac702a1b537790
Mary Kang <mary.kang09@gmail.com>
2020-02-12 00:00:30 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -9,7 +9,8 @@ function addElementToBeginningOfArray (array,element){
 function destructivelyAddElementToBeginningOfArray (array,element){
   var array = [1]
   array.unshift("foo")
-  }
+  return array
+}
 
 function addElementToEndOfArray (array, element){
   var array = [1]
